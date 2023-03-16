2ed2acd7064764ec3d383da8d3378c16741373cf
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-16 19:17:46 -0400

    Update webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,9 +34,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'This text editor works even offline!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
